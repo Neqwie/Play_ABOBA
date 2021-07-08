f2f94726b648caf04ed0d0d96bc817a1eeb44d1f
Neqwie <nikita220088@yandex.ru>
2021-07-08 11:04:45 +0300

    Небольшие объяснения и 0 фиксов

--- src/com/company/Rich.java
+++ src/com/company/Rich.java
@@ -3,20 +3,20 @@ package com.company;
 import java.util.Scanner;
 
 public class Rich {
-    public static void main (String[] args){
-System.out.println();
-System.out .println("-=[Тест на Абобу]=-");
-        Scanner SPID = new Scanner(System.in);
-        String str = SPID.nextLine();
-        if(str.equals("1")){
-            System.out.println("-=[Ты пидор]=-");
-        }
-        else if (str.equals("5")){
-            System.out.println("⚠-=[Данила Абоба]=-⚠");
-        }
-        else{
-            System.out.println("-=[Ты не угадал]=-");
-            System.out.println("       ...");
+    public static void main (String[] args){// Это хйня какая-то не шарю, но работает
+    System.out.println();//Пропуск строчик для красоты
+    System.out .println("-=[Тест на Абобу]=-");//Вывод надписи(Начальный экран)
+    Scanner SPID = new Scanner(System.in);//Сканер make
+    String str = SPID.nextLine();//Назначение переменной
+    if(str.equals("1")){// 1 УО при значении в скобках выполняектся команда ниже
+    System.out.println("-=[Ты пидор]=-");//Вывод команды при усл.
+    }
+    else if (str.equals("5")){//УО при значении в скобках(5) выводится надпись ниже(если не выполняется команда выше)
+    System.out.println("⚠-=[Данила Абоба]=-⚠");//Вывод надписи
+    }
+    else{// УО при котором выполняется команда если отсальные нет
+    System.out.println("-=[Ты не угадал]=-");//Вывод надписи
+    System.out.println("       ...");//Вывод конца игры
         }
     }
 }
