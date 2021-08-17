914b93c9de2ca866937cb559a1c05a995d998f20
Neqwie <nikita220088@yandex.ru>
2021-08-17 07:47:15 +0300

    Upgraded GAME ABOBA

--- src/com/company/Rich.java
+++ src/com/company/Rich.java
@@ -1,20 +1,23 @@
 package com.company;
 
+import java.awt.*;
 import java.util.Scanner;
 
 public class Rich {
     public static void main (String[] args){// Это хйня какая-то не шарю, но работает
+        final String TEXT_BLUE = "\u001B[34m";
+        final String TEXT_RED = "\u001B[31m";
     System.out.println();//Пропуск строчик для красоты
-    System.out .println("-=[Тест на Абобу]=-");//Вывод надписи(Начальный экран)
+    System.out .println(TEXT_BLUE + "-=[Тест на Абобу]=-");//Вывод надписи(Начальный экран)
     Scanner SPID = new Scanner(System.in);//Сканер make
     String str = SPID.nextLine();//Назначение переменной
     if(str.equals("1")){// 1 УО при значении в скобках выполняектся команда ниже
-    System.out.println("-=[Ты пидор]=-");//Вывод команды при усл.
+    System.out.println(TEXT_RED + "-=[Ты пидор]=-");//Вывод команды при усл.
     }
     else if (str.equals("5")){//УО при значении в скобках(5) выводится надпись ниже(если не выполняется команда выше)
-    System.out.println("⚠-=[Данила Абоба]=-⚠");//Вывод надписи
+    System.out.println(TEXT_BLUE + "⚠-=[Данила Абоба]=-⚠");//Вывод надписи
     }
-    else{// УО при котором выполняется команда если отсальные нет
+    else{// УО при котором выполняется команда если отсальные 5нет
     System.out.println("-=[Ты не угадал]=-");//Вывод надписи
     System.out.println("       ...");//Вывод конца игры
         }
